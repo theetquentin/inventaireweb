e8ba50075630764786969fae62084d12fd3801e2
theetquentin <quentin.theet@gmail.com>
2023-02-05 14:07:09 +0100

    Premier commit du frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <Inventaire/>
+  </div>
+</template>
+
+<script>
+import Inventaire from './components/Inventaire'
+
+export default {
+  name: 'App',
+  components: {
+    Inventaire
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+</style>
